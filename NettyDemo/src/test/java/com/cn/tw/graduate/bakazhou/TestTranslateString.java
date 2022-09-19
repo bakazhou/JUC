f7cb017d09529adfea397fa081db4c6715c64668
bakazhou <706515174@qq.com>
2022-09-19 14:15:51 +0800

    feat: add TestTranslateString

---
+++ NettyDemo/src/test/java/com/cn/tw/graduate/bakazhou/TestTranslateString.java
@@ -0,0 +1,66 @@
+package com.cn.tw.graduate.bakazhou;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class TestTranslateString {
+    @Test
+    void method1() {
+        // 准备两个字符串
+        String str1 = "hello";
+        String str2 = "";
+
+
+        ByteBuffer buffer1 = ByteBuffer.allocate(16);
+        // 通过字符串的getByte方法获得字节数组，放入缓冲区中
+        buffer1.put(str1.getBytes());
+        ByteBufferUtil.debugAll(buffer1);
+
+        // 将缓冲区中的数据转化为字符串
+        // 切换模式
+        buffer1.flip();
+
+        // 通过StandardCharsets解码，获得CharBuffer，再通过toString获得字符串
+        str2 = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println(str2);
+        ByteBufferUtil.debugAll(buffer1);
+    }
+
+    @Test
+    void method2() {
+        // 准备两个字符串
+        String str1 = "hello";
+        String str2 = "";
+
+        // 通过StandardCharsets的encode方法获得ByteBuffer
+        // 此时获得的ByteBuffer为读模式，无需通过flip切换模式
+        ByteBuffer buffer1 = StandardCharsets.UTF_8.encode(str1);
+        ByteBufferUtil.debugAll(buffer1);
+
+        // 将缓冲区中的数据转化为字符串
+        // 通过StandardCharsets解码，获得CharBuffer，再通过toString获得字符串
+        str2 = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println(str2);
+        ByteBufferUtil.debugAll(buffer1);
+    }
+
+    @Test
+    void method3() {
+        // 准备两个字符串
+        String str1 = "hello";
+        String str2 = "";
+
+        // 通过StandardCharsets的encode方法获得ByteBuffer
+        // 此时获得的ByteBuffer为读模式，无需通过flip切换模式
+        ByteBuffer buffer1 = ByteBuffer.wrap(str1.getBytes());
+        ByteBufferUtil.debugAll(buffer1);
+
+        // 将缓冲区中的数据转化为字符串
+        // 通过StandardCharsets解码，获得CharBuffer，再通过toString获得字符串
+        str2 = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println(str2);
+        ByteBufferUtil.debugAll(buffer1);
+    }
+}
