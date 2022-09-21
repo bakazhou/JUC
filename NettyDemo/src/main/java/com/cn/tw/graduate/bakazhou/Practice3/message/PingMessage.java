282a38ea0e2f085e3817b4cfa5a59d2ff8f1f86b
bakazhou <706515174@qq.com>
2022-09-21 14:45:41 +0800

    feat: init practice3

---
+++ NettyDemo/src/main/java/com/cn/tw/graduate/bakazhou/Practice3/message/PingMessage.java
@@ -0,0 +1,8 @@
+package com.cn.tw.graduate.bakazhou.Practice3.message;
+
+public class PingMessage extends Message {
+    @Override
+    public int getMessageType() {
+        return PingMessage;
+    }
+}
---
+++ NettyDemo/src/main/java/com/cn/tw/graduate/bakazhou/Practice3/message/PongMessage.java
@@ -0,0 +1,8 @@
+package com.cn.tw.graduate.bakazhou.Practice3.message;
+
+public class PongMessage extends Message {
+    @Override
+    public int getMessageType() {
+        return PongMessage;
+    }
+}
